8a2416cf61aea86b54af29d2c895008dfaa25e2b
Marco Roth <marco.roth@intergga.ch>
2022-11-30 00:52:32 +0100

    bump to `0.1.6`

--- app/assets/javascripts/turbo_power.js
+++ app/assets/javascripts/turbo_power.js
@@ -1,15 +1,15 @@
-import "@hotwired/turbo";
-
 function t() {
-  let t = JSON.parse(this.templateContent.textContent), {id: e, selector: o, receiver: r, method: n, args: i} = t, s = [ self ];
-  if ("dispatchEvent" === (o && (s = Array.from(document.querySelectorAll(o))), r && (s = s.map((t => {
-    let e = t, o = r.split(".");
+  const t = JSON.parse(this.templateContent.textContent), {id: e, selector: o, receiver: r, method: n, args: i} = t;
+  let s = [ self ];
+  return o && (s = Array.from(document.querySelectorAll(o))), r && (s = s.map((t => {
+    let e = t;
+    const o = r.split(".");
     for (;o.length > 0; ) e = e[o.shift()];
     return e;
-  }))), n)) {
-    let t = new CustomEvent(i[0], i[1] || {});
-    s.forEach((e => e.dispatchEvent(t)));
-  } else s.forEach((t => t[n].apply(t, i)));
+  }))), "dispatchEvent" === n ? function(t, e, o = {}) {
+    const r = new CustomEvent(e, o);
+    t.forEach((t => t.dispatchEvent(r)));
+  }(s, i[0], i[1] || {}) : n.endsWith("=") ? s.forEach((t => t[n.slice(0, -1).trim()] = i[0])) : void s.forEach((t => t[n].apply(t, i)));
 }
 
 var e, o = {
@@ -39,7 +39,7 @@ function c(t, e, o) {
   t[o] !== e[o] && (t[o] = e[o], t[o] ? t.setAttribute(o, "") : t.removeAttribute(o));
 }
 
-var l = {
+var u = {
   OPTION: function(t, e) {
     var o = t.parentNode;
     if (o) {
@@ -80,7 +80,7 @@ var l = {
   }
 };
 
-function u() {}
+function l() {}
 
 function d(t) {
   if (t) return t.getAttribute && t.getAttribute("id") || t.id;
@@ -91,25 +91,25 @@ var f = function(t, e, o) {
     var n = e;
     (e = r.createElement("html")).innerHTML = n;
   } else e = s(e);
-  var i = o.getNodeKey || d, c = o.onBeforeNodeAdded || u, f = o.onNodeAdded || u, h = o.onBeforeElUpdated || u, m = o.onElUpdated || u, b = o.onBeforeNodeDiscarded || u, p = o.onNodeDiscarded || u, g = o.onBeforeElChildrenUpdated || u, v = !0 === o.childrenOnly, _ = Object.create(null), A = [];
+  var i = o.getNodeKey || d, c = o.onBeforeNodeAdded || l, f = o.onNodeAdded || l, h = o.onBeforeElUpdated || l, m = o.onElUpdated || l, b = o.onBeforeNodeDiscarded || l, g = o.onNodeDiscarded || l, p = o.onBeforeElChildrenUpdated || l, _ = !0 === o.childrenOnly, v = Object.create(null), A = [];
   function E(t) {
     A.push(t);
   }
   function T(t, e) {
     if (1 === t.nodeType) for (var o = t.firstChild; o; ) {
       var r = void 0;
-      e && (r = i(o)) ? E(r) : (p(o), o.firstChild && T(o, e)), o = o.nextSibling;
+      e && (r = i(o)) ? E(r) : (g(o), o.firstChild && T(o, e)), o = o.nextSibling;
     }
   }
   function w(t, e, o) {
-    !1 !== b(t) && (e && e.removeChild(t), p(t), T(t, o));
+    !1 !== b(t) && (e && e.removeChild(t), g(t), T(t, o));
   }
   function y(t) {
     f(t);
     for (var e = t.firstChild; e; ) {
       var o = e.nextSibling, r = i(e);
       if (r) {
-        var n = _[r];
+        var n = v[r];
         n && a(e, n) ? (e.parentNode.replaceChild(n, e), C(n, e)) : y(e);
       } else y(e);
       e = o;
@@ -117,7 +117,7 @@ var f = function(t, e, o) {
   }
   function C(t, e, o) {
     var n = i(e);
-    if (n && delete _[n], !o) {
+    if (n && delete v[n], !o) {
       if (!1 === h(t, e)) return;
       if (function(t, e) {
         var o, r, n, i, s = e.attributes;
@@ -125,35 +125,35 @@ var f = function(t, e, o) {
           for (var a = s.length - 1; a >= 0; a--) r = (o = s[a]).name, n = o.namespaceURI, 
           i = o.value, n ? (r = o.localName || r, t.getAttributeNS(n, r) !== i && ("xmlns" === o.prefix && (r = o.name), 
           t.setAttributeNS(n, r, i))) : t.getAttribute(r) !== i && t.setAttribute(r, i);
-          for (var c = t.attributes, l = c.length - 1; l >= 0; l--) r = (o = c[l]).name, (n = o.namespaceURI) ? (r = o.localName || r, 
+          for (var c = t.attributes, u = c.length - 1; u >= 0; u--) r = (o = c[u]).name, (n = o.namespaceURI) ? (r = o.localName || r, 
           e.hasAttributeNS(n, r) || t.removeAttributeNS(n, r)) : e.hasAttribute(r) || t.removeAttribute(r);
         }
-      }(t, e), m(t), !1 === g(t, e)) return;
+      }(t, e), m(t), !1 === p(t, e)) return;
     }
     "TEXTAREA" !== t.nodeName ? function(t, e) {
-      var o, n, s, u, d, f = e.firstChild, h = t.firstChild;
+      var o, n, s, l, d, f = e.firstChild, h = t.firstChild;
       t: for (;f; ) {
-        for (u = f.nextSibling, o = i(f); h; ) {
+        for (l = f.nextSibling, o = i(f); h; ) {
           if (s = h.nextSibling, f.isSameNode && f.isSameNode(h)) {
-            f = u, h = s;
+            f = l, h = s;
             continue t;
           }
           n = i(h);
           var m = h.nodeType, b = void 0;
-          if (m === f.nodeType && (1 === m ? (o ? o !== n && ((d = _[o]) ? s === d ? b = !1 : (t.insertBefore(d, h), 
+          if (m === f.nodeType && (1 === m ? (o ? o !== n && ((d = v[o]) ? s === d ? b = !1 : (t.insertBefore(d, h), 
           n ? E(n) : w(h, t, !0), h = d) : b = !1) : n && (b = !1), (b = !1 !== b && a(h, f)) && C(h, f)) : 3 !== m && 8 != m || (b = !0, 
           h.nodeValue !== f.nodeValue && (h.nodeValue = f.nodeValue))), b) {
-            f = u, h = s;
+            f = l, h = s;
             continue t;
           }
           n ? E(n) : w(h, t, !0), h = s;
         }
-        if (o && (d = _[o]) && a(d, f)) t.appendChild(d), C(d, f); else {
-          var p = c(f);
-          !1 !== p && (p && (f = p), f.actualize && (f = f.actualize(t.ownerDocument || r)), 
+        if (o && (d = v[o]) && a(d, f)) t.appendChild(d), C(d, f); else {
+          var g = c(f);
+          !1 !== g && (g && (f = g), f.actualize && (f = f.actualize(t.ownerDocument || r)), 
           t.appendChild(f), y(f));
         }
-        f = u, h = s;
+        f = l, h = s;
       }
       !function(t, e, o) {
         for (;e; ) {
@@ -161,18 +161,18 @@ var f = function(t, e, o) {
           (o = i(e)) ? E(o) : w(e, t, !0), e = r;
         }
       }(t, h, n);
-      var g = l[t.nodeName];
-      g && g(t, e);
-    }(t, e) : l.TEXTAREA(t, e);
+      var p = u[t.nodeName];
+      p && p(t, e);
+    }(t, e) : u.TEXTAREA(t, e);
   }
   !function t(e) {
     if (1 === e.nodeType || 11 === e.nodeType) for (var o = e.firstChild; o; ) {
       var r = i(o);
-      r && (_[r] = o), t(o), o = o.nextSibling;
+      r && (v[r] = o), t(o), o = o.nextSibling;
     }
   }(t);
   var S, N, x = t, P = x.nodeType, O = e.nodeType;
-  if (!v) if (1 === P) 1 === O ? a(t, e) || (p(t), x = function(t, e) {
+  if (!_) if (1 === P) 1 === O ? a(t, e) || (g(t), x = function(t, e) {
     for (var o = t.firstChild; o; ) {
       var r = o.nextSibling;
       e.appendChild(o), o = r;
@@ -183,14 +183,14 @@ var f = function(t, e, o) {
     x;
     x = e;
   }
-  if (x === e) p(t); else {
+  if (x === e) g(t); else {
     if (e.isSameNode && e.isSameNode(x)) return;
-    if (C(x, e, v), A) for (var k = 0, U = A.length; k < U; k++) {
-      var L = _[A[k]];
+    if (C(x, e, _), A) for (var k = 0, U = A.length; k < U; k++) {
+      var L = v[A[k]];
       L && w(L, L.parentNode, !1);
     }
   }
-  return !v && x !== t && t.parentNode && (x.actualize && (x = x.actualize(t.ownerDocument || r)), 
+  return !_ && x !== t && t.parentNode && (x.actualize && (x = x.actualize(t.ownerDocument || r)), 
   t.parentNode.replaceChild(x, t)), x;
 };
 
@@ -214,27 +214,27 @@ function b() {
   t ? this.targetElements.forEach((e => e.removeAttribute(t))) : console.warn('[TurboPower] no "attribute" provided for Turbo Streams operation "remove_attribute"');
 }
 
-function p() {
+function g() {
   var t;
   const e = ((null === (t = this.getAttribute("classes")) || void 0 === t ? void 0 : t.split(" ")) || []).filter((t => t.length > 0));
   e.length > 0 ? this.targetElements.forEach((t => t.classList.remove(...e))) : console.warn('[TurboPower] no "classes" provided for Turbo Streams operation "remove_css_class"');
 }
 
-function g() {
+function p() {
   const t = this.getAttribute("attribute"), e = this.getAttribute("value") || "";
   t ? this.targetElements.forEach((o => o.setAttribute(t, e))) : console.warn('[TurboPower] no "attribute" provided for Turbo Streams operation "set_attribute"');
 }
 
-function v() {
+function _() {
   const t = this.getAttribute("attribute"), e = this.getAttribute("value") || "";
   t ? this.targetElements.forEach((o => o.dataset[function(t) {
     return t.replace(/(?:[_-])([a-z0-9])/g, ((t, e) => e.toUpperCase()));
   }(t)] = e)) : console.warn('[TurboPower] no "attribute" provided for Turbo Streams operation "set_dataset_attribute"');
 }
 
-function _() {
-  const t = this.getAttribute("name");
-  t ? this.targetElements.forEach((e => e[t] = value)) : console.error('[TurboPower] no "name" provided for Turbo Streams operation "set_property"');
+function v() {
+  const t = this.getAttribute("name"), e = this.getAttribute("value") || "";
+  t ? this.targetElements.forEach((o => o[t] = e)) : console.error('[TurboPower] no "name" provided for Turbo Streams operation "set_property"');
 }
 
 function A() {
@@ -253,13 +253,13 @@ function T() {
 }
 
 function w(t) {
-  t.add_css_class = m, t.remove_css_class = p, t.remove_attribute = b, t.set_attribute = g, 
-  t.set_dataset_attribute = v, t.set_property = _, t.set_style = A, t.set_styles = E, 
+  t.add_css_class = m, t.remove_css_class = g, t.remove_attribute = b, t.set_attribute = p, 
+  t.set_dataset_attribute = _, t.set_property = v, t.set_style = A, t.set_styles = E, 
   t.set_value = T;
 }
 
 function y() {
-  const t = this.getAttribute("url") || "/", e = this.getAttribute("action") || "advance";
+  const t = this.getAttribute("url") || "/", e = this.getAttribute("turbo_action") || "advance";
   "true" === this.getAttribute("turbo") ? (window.Turbo && window.Turbo.visit(t, {
     action: e
   }), window.Turbolinks && window.Turbolinks.visit(t, {
@@ -294,12 +294,16 @@ function O() {
   e || (e = document.createElement("title"), document.head.appendChild(e)), e.textContent = t;
 }
 
-function k(t) {
-  t.redirect_to = y, t.reload = C, t.scroll_into_view = S, t.set_cookie = N, t.set_cookie_item = x, 
-  t.set_focus = P, t.set_title = O;
+function k() {
+  window.Turbo.cache.clear();
 }
 
-function U() {
+function U(t) {
+  t.redirect_to = y, t.reload = C, t.scroll_into_view = S, t.set_cookie = N, t.set_cookie_item = x, 
+  t.set_focus = P, t.set_title = O, t.turbo_clear_cache = k;
+}
+
+function L() {
   const t = this.getAttribute("parent");
   if (t) {
     const e = document.querySelector(t);
@@ -307,27 +311,27 @@ function U() {
   } else console.error('[TurboPower] no "parent" selector provided for Turbo Streams operation "graft"');
 }
 
-function L() {
+function I() {
   const t = this.templateContent.textContent || "";
   this.targetElements.forEach((e => e.innerHTML = t));
 }
 
-function I() {
+function R() {
   const t = this.getAttribute("position") || "beforebegin", e = this.templateContent.textContent || "";
   this.targetElements.forEach((o => o.insertAdjacentHTML(t, e)));
 }
 
-function R() {
+function V() {
   const t = this.getAttribute("position") || "beforebegin", e = this.getAttribute("text") || "";
   this.targetElements.forEach((o => o.insertAdjacentText(t, e)));
 }
 
-function V() {
+function z() {
   const t = this.templateContent.textContent || "";
   this.targetElements.forEach((e => e.outerHTML = t));
 }
 
-function z() {
+function H() {
   const t = this.getAttribute("name"), e = this.getAttribute("content") || "";
   if (t) {
     let o = document.head.querySelector(`meta[name='${t}']`);
@@ -336,31 +340,31 @@ function z() {
   } else console.error('[TurboPower] no "name" provided for Turbo Streams operation "set_meta"');
 }
 
-function H() {
+function M() {
   const t = this.getAttribute("text") || "";
   this.targetElements.forEach((e => e.textContent = t));
 }
 
-function M(t) {
-  t.graft = U, t.inner_html = L, t.insert_adjacent_html = I, t.insert_adjacent_text = R, 
-  t.outer_html = V, t.text_content = H, t.set_meta = z;
+function j(t) {
+  t.graft = L, t.inner_html = I, t.insert_adjacent_html = R, t.insert_adjacent_text = V, 
+  t.outer_html = z, t.text_content = M, t.set_meta = H;
 }
 
-function j() {
+function B() {
   const t = this.getAttribute("message"), e = this.getAttribute("level") || "log";
   console[e](t);
 }
 
-function B() {
+function D() {
   const t = JSON.parse(this.getAttribute("data") || "[]"), e = JSON.parse(this.getAttribute("columns") || "[]");
   console.table(t, e);
 }
 
-function D(t) {
-  t.console_log = j, t.console_table = B;
+function q(t) {
+  t.console_log = B, t.console_table = D;
 }
 
-function q() {
+function J() {
   const t = this.getAttribute("name");
   let e = null;
   try {
@@ -381,11 +385,11 @@ function q() {
   }
 }
 
-function J(t) {
-  t.dispatch_event = q;
+function $(t) {
+  t.dispatch_event = J;
 }
 
-function $() {
+function F() {
   const t = this.getAttribute("url"), e = this.getAttribute("state"), o = this.getAttribute("title") || "";
   window.history.pushState(e, o, t);
 }
@@ -395,61 +399,76 @@ function X() {
   window.history.replaceState(e, o, t);
 }
 
-function F() {
+function G() {
   const t = Number(this.getAttribute("delta")) || 0;
   window.history.go(t);
 }
 
-function G(t) {
-  t.push_state = $, t.replace_state = X, t.history_go = F;
+function K(t) {
+  t.push_state = F, t.replace_state = X, t.history_go = G;
 }
 
-function K(t) {
+function W(t) {
   return "session" === t ? window.sessionStorage : window.localStorage;
 }
 
 function Y() {
-  K(this.getAttribute("type")).clear();
+  W(this.getAttribute("type")).clear();
 }
 
 function Q() {
   const t = this.getAttribute("key"), e = this.getAttribute("value") || "", o = this.getAttribute("type");
-  t ? K(o).setItem(t, e) : console.warn('[TurboPower] no "key" provided for Turbo Streams operation "set_storage_item"');
+  t ? W(o).setItem(t, e) : console.warn('[TurboPower] no "key" provided for Turbo Streams operation "set_storage_item"');
 }
 
-function W() {
+function Z() {
   const t = this.getAttribute("key"), e = this.getAttribute("type");
-  t ? K(e).removeItem(t) : console.warn('[TurboPower] no "key" provided for Turbo Streams operation "remove_storage_item"');
-}
-
-function Z(t) {
-  t.clear_storage = Y, t.set_storage_item = Q, t.remove_storage_item = W;
+  t ? W(e).removeItem(t) : console.warn('[TurboPower] no "key" provided for Turbo Streams operation "remove_storage_item"');
 }
 
 function tt(t) {
-  w(t), k(t), D(t), M(t), J(t), G(t), Z(t);
+  t.clear_storage = Y, t.set_storage_item = Q, t.remove_storage_item = Z;
 }
 
-function et(t) {
-  t.morph = h, o.initialize(t), tt(t);
+function et() {
+  this.targetElements.forEach((t => t.reload()));
 }
 
-function ot(t, e, o) {
+function ot() {
+  const t = this.getAttribute("src");
+  this.targetElements.forEach((e => e.src = t));
+}
+
+function rt(t) {
+  t.turbo_frame_reload = et, t.turbo_frame_set_src = ot;
+}
+
+function nt(t) {
+  w(t), U(t), q(t), j(t), $(t), K(t), tt(t), rt(t);
+}
+
+function it(t) {
+  (t => {
+    t.morph = h;
+  })(t), o.initialize(t), nt(t);
+}
+
+function st(t, e, o) {
   o[t] = e;
 }
 
-var rt = {
-  initialize: et,
-  register: ot,
+var at = {
+  initialize: it,
+  register: st,
   Actions: Object.freeze({
     __proto__: null,
-    register: tt,
+    register: nt,
     add_css_class: m,
     remove_attribute: b,
-    remove_css_class: p,
-    set_attribute: g,
-    set_dataset_attribute: v,
-    set_property: _,
+    remove_css_class: g,
+    set_attribute: p,
+    set_dataset_attribute: _,
+    set_property: v,
     set_style: A,
     set_styles: E,
     set_value: T,
@@ -461,31 +480,35 @@ var rt = {
     set_cookie_item: x,
     set_focus: P,
     set_title: O,
-    registerBrowserActions: k,
-    console_log: j,
-    console_table: B,
-    registerDebugActions: D,
-    graft: U,
-    inner_html: L,
-    insert_adjacent_html: I,
-    insert_adjacent_text: R,
-    outer_html: V,
-    set_meta: z,
-    text_content: H,
-    registerDOMActions: M,
-    dispatch_event: q,
-    registerEventsActions: J,
-    push_state: $,
+    turbo_clear_cache: k,
+    registerBrowserActions: U,
+    console_log: B,
+    console_table: D,
+    registerDebugActions: q,
+    graft: L,
+    inner_html: I,
+    insert_adjacent_html: R,
+    insert_adjacent_text: V,
+    outer_html: z,
+    set_meta: H,
+    text_content: M,
+    registerDOMActions: j,
+    dispatch_event: J,
+    registerEventsActions: $,
+    push_state: F,
     replace_state: X,
-    history_go: F,
-    registerHistoryActions: G,
+    history_go: G,
+    registerHistoryActions: K,
     clear_storage: Y,
     set_storage_item: Q,
-    remove_storage_item: W,
-    registerStorageActions: Z
+    remove_storage_item: Z,
+    registerStorageActions: tt,
+    turbo_frame_reload: et,
+    turbo_frame_set_src: ot,
+    registerTurboFrameActions: rt
   })
 };
 
-var TurboPower = rt;
+var TurboPower = at;
 
 export { TurboPower as default };
