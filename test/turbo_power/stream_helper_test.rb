977ca54124d8bb0c7095770991aa14a15cc9ca95
Marco Roth <marco.roth@intergga.ch>
2023-01-04 06:24:41 +0100

    add test

--- test/turbo_power/stream_helper_test.rb
+++ test/turbo_power/stream_helper_test.rb
@@ -12,5 +12,11 @@ module TurboPower
 
       assert_dom_equal stream, turbo_stream.dispatch_event("#element", "custom-event", detail: { count: 1, type: "custom", enabled: true, ids: [1, 2, 3] })
     end
+
+    test "reset_form" do
+      stream = %(<turbo-stream action="reset_form" targets="#form"></turbo-stream>)
+
+      assert_dom_equal stream, turbo_stream.reset_form("#form")
+    end
   end
 end
