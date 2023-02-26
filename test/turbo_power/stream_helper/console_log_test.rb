a3f9ef9628b83472748b4fa531d7aa76c4016800
Marco Roth <marco.roth@intergga.ch>
2023-02-26 04:34:07 +0100

    Add test for using both args and kwargs

--- test/turbo_power/stream_helper/console_log_test.rb
+++ test/turbo_power/stream_helper/console_log_test.rb
@@ -35,6 +35,12 @@ module TurboPower
         assert_dom_equal stream, turbo_stream.console_log("Message", level: "warn")
       end
 
+      test "console_log with message/level as positional arg and kwarg" do
+        stream = %(<turbo-stream message="Better Message" level="error" action="console_log"><template></template></turbo-stream>)
+
+        assert_dom_equal stream, turbo_stream.console_log("Message", "warn", message: "Better Message", level: "error")
+      end
+
       test "console_log with additional arguments" do
         stream = %(<turbo-stream level="warn" message="Message" something="else" action="console_log"><template></template></turbo-stream>)
 
