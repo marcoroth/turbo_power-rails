8ef3b50c3851dc23dacec945ce027713991cff9f
Marco Roth <marco.roth@intergga.ch>
2023-03-03 02:32:35 +0100

    Add tests for `replace_state` action and add kwarg support

---
+++ test/turbo_power/stream_helper/replace_state.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+module TurboPower
+  module StreamHelper
+    class ReplaceStateTest < StreamHelperTestCase
+      test "replace_state" do
+        stream = %(<turbo-stream action="replace_state" url="/users/1" title="User 1" state="user-1"><template></template></turbo-stream>)
+
+        assert_dom_equal stream, turbo_stream.replace_state("/users/1", "User 1", "user-1")
+      end
+
+      test "replace_state with state as kwargs" do
+        stream = %(<turbo-stream action="replace_state" url="/users/1" title="User 1" state="user-1"><template></template></turbo-stream>)
+
+        assert_dom_equal stream, turbo_stream.replace_state("/users/1", "User 1", state: "user-1")
+      end
+
+      test "replace_state with title and state as kwargs" do
+        stream = %(<turbo-stream action="replace_state" url="/users/1" title="User 1" state="user-1"><template></template></turbo-stream>)
+
+        assert_dom_equal stream, turbo_stream.replace_state("/users/1", title: "User 1", state: "user-1")
+      end
+
+      test "replace_state with url, title and state as kwargs" do
+        stream = %(<turbo-stream action="replace_state" url="/users/1" title="User 1" state="user-1"><template></template></turbo-stream>)
+
+        assert_dom_equal stream, turbo_stream.replace_state(url: "/users/1", title: "User 1", state: "user-1")
+      end
+
+      test "replace_state with state, url and title as kwargs" do
+        stream = %(<turbo-stream action="replace_state" url="/users/1" title="User 1" state="user-1"><template></template></turbo-stream>)
+
+        assert_dom_equal stream, turbo_stream.replace_state(state: "user-1", url: "/users/1", title: "User 1")
+      end
+
+      test "replace_state with targets/value as positional arg and kwarg" do
+        stream = %(<turbo-stream action="replace_state" url="/users/2" title="User 2" state="user-2"><template></template></turbo-stream>)
+
+        assert_dom_equal stream, turbo_stream.replace_state("/users/1", "User 1", "user-1", url: "/users/2", title: "User 2", state: "user-2")
+      end
+
+      test "replace_state with additional arguments" do
+        stream = %(<turbo-stream action="replace_state" url="/users/1" title="User 1" state="user-1" something="else"><template></template></turbo-stream>)
+
+        assert_dom_equal stream, turbo_stream.replace_state("/users/1", title: "User 1", state: "user-1", something: "else")
+      end
+    end
+  end
+end
