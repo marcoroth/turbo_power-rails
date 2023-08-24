421dacab352a767ca10a386eba2e235eb5570d8e
Marco Roth <marco.roth@intergga.ch>
2023-08-24 15:01:00 +0200

    Add tests for all `redirect_to` permutations with `turbo_frame`

--- test/turbo_power/stream_helper/redirect_to_test.rb
+++ test/turbo_power/stream_helper/redirect_to_test.rb
@@ -17,7 +17,7 @@ module TurboPower
         assert_dom_equal stream, turbo_stream.redirect_to("http://localhost:8080", turbo: false)
       end
 
-      test "redirect_to with action=replace" do
+      test "redirect_to with turbo_action=replace" do
         stream = %(<turbo-stream turbo-action="replace" url="http://localhost:8080" action="redirect_to"><template></template></turbo-stream>)
 
         assert_dom_equal stream, turbo_stream.redirect_to("http://localhost:8080", "replace")
@@ -29,16 +29,28 @@ module TurboPower
         assert_dom_equal stream, turbo_stream.redirect_to("http://localhost:8080", turbo_action: "replace")
       end
 
-      test "redirect_to all kwargs" do
-        stream = %(<turbo-stream turbo-action="replace" turbo="true" url="http://localhost:8080" action="redirect_to"><template></template></turbo-stream>)
+      test "redirect_to with turbo_action=replace and turbo_frame=modals as positional arguments" do
+        stream = %(<turbo-stream turbo-action="replace" turbo-frame="modals" url="http://localhost:8080" action="redirect_to"><template></template></turbo-stream>)
 
-        assert_dom_equal stream, turbo_stream.redirect_to(url: "http://localhost:8080", turbo_action: "replace", turbo: true)
+        assert_dom_equal stream, turbo_stream.redirect_to("http://localhost:8080", "replace", "modals")
       end
 
-      test "redirect_to with turbo_frame" do
-        stream = %(<turbo-stream action="redirect_to" turbo-action="advance" turbo-frame="modals" url="http://localhost:8080"><template></template></turbo-stream>)
+      test "redirect_to with turbo_action=replace as positional argument and turbo_frame=modals as kwarg" do
+        stream = %(<turbo-stream turbo-action="replace" turbo-frame="modals" url="http://localhost:8080" action="redirect_to"><template></template></turbo-stream>)
 
-        assert_dom_equal stream, turbo_stream.redirect_to("http://localhost:8080", turbo_frame: "modals")
+        assert_dom_equal stream, turbo_stream.redirect_to("http://localhost:8080", "replace", turbo_frame: "modals")
+      end
+
+      test "redirect_to with turbo_action=replace and turbo_frame=modals as kwarg" do
+        stream = %(<turbo-stream turbo-action="replace" turbo-frame="modals" url="http://localhost:8080" action="redirect_to"><template></template></turbo-stream>)
+
+        assert_dom_equal stream, turbo_stream.redirect_to("http://localhost:8080", turbo_action: "replace", turbo_frame: "modals")
+      end
+
+      test "redirect_to all kwargs" do
+        stream = %(<turbo-stream turbo-action="replace" turbo-frame="modals" turbo="true" url="http://localhost:8080" action="redirect_to"><template></template></turbo-stream>)
+
+        assert_dom_equal stream, turbo_stream.redirect_to(url: "http://localhost:8080", turbo_action: "replace", turbo_frame: "modals", turbo: true)
       end
     end
   end
