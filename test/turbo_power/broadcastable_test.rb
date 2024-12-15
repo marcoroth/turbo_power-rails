ee52e9a930cbd1b36470b66b962aa236aeedcd01
Marco Roth <marco.roth@intergga.ch>
2024-12-15 13:13:06 +0100

    Require `turbo-rails/broadcastable/test_helper`

--- test/turbo_power/broadcastable_test.rb
+++ test/turbo_power/broadcastable_test.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
+require "turbo/broadcastable/test_helper"
 require "test_helper"
-require "turbo-rails"
 
 module TurboPower
   class BroadcastableTest < ActionCable::Channel::TestCase
